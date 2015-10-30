dc62dd5c7a6ffb895f7a6c2139ba6fa36a49b65b
Chase McCarthy <chase@code0100fun.com>
2015-10-30 14:43:29 -0400

    Add prepend task for developemnt/test env

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 /* jshint node: true */
 'use strict';
+var assetPrepend = require('./lib/asset-prepend');
 
 module.exports = {
-  name: 'ember-cli-rails-routes'
-};
+  name: 'ember-cli-rails-routes',
+  initializeOptions: function() {
+    var defaultOptions = {
+      enabled: this.app.env === 'development' || this.app.env === 'test',
+      exclude: [],
+      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map'],
+      prepend: '',
+      replaceExtensions: ['html', 'css', 'js']
+    };
+
+    var options = this.project.config(this.app.env)[this.name];
+
+    // { 'ember-cli-rails-routes': false } to disable
+    if (options === false) {
+      this.options = { enabled: false };
+    } else {
+      this.options = options || {};
+    }
+
+    this.app.options[this.name] = this.options;
+
+    for (var option in defaultOptions) {
+      if (!this.options.hasOwnProperty(option)) {
+        this.options[option] = defaultOptions[option];
+      }
+    }
+  },
+  postprocessTree: function (type, tree) {
+    if (type === 'all' && this.options.enabled) {
+      tree = assetPrepend(tree, this.options);
+    }
+    return tree;
+  },
+  included: function (app) {
+    this.app = app;
+    this.initializeOptions();
+  },
+  treeFor: function() {}
+}
